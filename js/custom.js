19df9a203a33839c577aecf28e1d17aeb8e88ebf
Kajka09 <kajkastefaniak@gmail.com>
2017-03-03 23:40:34 +0100

    done

--- js/custom.js
+++ js/custom.js
@@ -6,36 +6,42 @@ $(function(){
 	var slideCount = document.querySelectorAll(".slide-show > .single-slide").length;
 	var slideWidth = 100/slideCount + "%";
 	var slideIndex = 0;
+	var currentSlideIndex = 0  ;
 
-	$(".slide-show").css("width", slideCount*100 +"%");
+$(".slide-show").css("width", slideCount*100 +"%");
 					   
-	$(".slide-show").find(".single-slide").each(function(this.index){
-		$(this).css({"margin-left":"parseInt(slideWidth)*index", "width": "slideWidth"})
+$(".slide-show").find(".single-slide").each(function(index){
+	$(this).css({"margin-left":parseInt(slideWidth)*index+'%', "width": "slideWidth"})
 	});
 	
 	
-	$(".prev-slide").click(function(e){
+$(".prev-slide").click(function(e){
+	e.preventDefault();
+	slide(currentSlideIndex-1);
+	currentSlideIndex--;
+});
+	
+$(".next-slide").click(function(e){
 		e.preventDefault();
-		slide(-1);
-	});
-	
-	$(".next-slide").click(function(e){
-		e.preventDefault();
-		slide(+1);
-	});
-	
-	var currentSlideIndex = 0;
-//	function slide ()
+		slide(currentSlideIndex+1);
+		currentSlideIndex++;
+});
 	
 	
-	var slideCaption =
-	slideCaption.hide;
+function slide(newSlideIndex){
+	if(newSlideIndex < 0 || newSlideIndex > slideCount) return;
+	
+	var slideCaption = $('.slider-caption')[newSlideIndex].innerHTML;
+		slideCaption.hide;
 		
-	var marginLeft = currentSlideIndex*-100+"%";
-        $('.slide-show').animate({"margin-left": marginLeft}, 2000, callBack)
+	var marginLeft = (newSlideIndex*(-100))+"%";
+        
+	$('.slide-show').animate({"margin-left": marginLeft}, 2000, callBack)
 								 
-		function callBack(){
-          $('.slide-show')slideCaption.fadeIn(1000);
-		}
+	function callBack(){
+    $('.slide-show').fadeIn(1000);
+	}
+}
+	
 			
-} );
+});
