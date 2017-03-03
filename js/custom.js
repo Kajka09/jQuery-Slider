d39af62d81c6df6dc84580de0cd8fcb332912ab0
Kajka09 <kajkastefaniak@gmail.com>
2017-03-03 10:32:51 +0100

    wersja 3.03

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,41 @@
 $(function(){
 	'use strict'
 	
-	function animacjaTrzecia(){
-		$("div").html("Gratulacje, animacja zakończona");
-	}
-	
-	function animacjaDruga(){
-		$("div").animate({"background-color":"blue"}, 5000, animacjaTrzecia);
-	};
-	
-	
-	$("button").click(function(){
-		$("div").animate({"width":"100px", "height":"100px", "margin-left": "108px"},3000, animacjaDruga);
-		
-		
+	var slider = document.getElementById("slider");
+	var slideShow = document.getElementsByClassName("slide-show");
+	var slideCount = document.querySelectorAll(".slide-show > .single-slide").length;
+	var slideWidth = 100/slideCount + "%";
+	var slideIndex = 0;
+
+	$(".slide-show").css("width", slideCount*100 +"%");
+					   
+	$(".slide-show").find(".single-slide").each(function(this.index){
+		$(this).css({"margin-left":"parseInt(slideWidth)*index", "width": "slideWidth"})
 	});
 	
 	
+	$(".prev-slide").click(function(e){
+		e.preventDefault();
+		slide(-1);
+	});
+	
+	$(".next-slide").click(function(e){
+		e.preventDefault();
+		slide(+1);
+	});
+	
+	var currentSlideIndex = 0;
+//	function slide ()
+	
+	
+	var slideCaption =
+	slideCaption.hide;
+		
+	var marginLeft = currentSlideIndex*-100+"%";
+        $('.slide-show').animate({"margin-left": marginLeft}, 2000, callBack)
+								 
+		function callBack(){
+          $('.slide-show')slideCaption.fadeIn(1000);
+		}
+			
 } );
